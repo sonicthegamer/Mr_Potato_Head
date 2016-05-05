585df51b3a07062425914f4a283345c24f2c9263
Student Temp Account <studenttempaccount@Administrators-iMac.local>
2016-05-05 15:38:11 -0400

    Moustache

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+$(document).ready(function(){console.log("js working");
 
+	$('#toggle').click(function(){
+		$('#moustache_image').toggle();
+	});
+
+	$('#btrigger').click(function(){
+		$("#potato").toggleClass('ColorC');
+	});
+});
